f9f4a3e480b760fd4c60bdffeeadad4f0280bc20
tranhuyhoangbka <hoangth92.nd@gmail.com>
2015-09-15 00:08:00 +0700

    modeling data

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,15 @@ class User < ActiveRecord::Base
   has_many :subscriptions, foreign_key: :follower_id, dependent: :destroy
   has_many :leaders, through: :subscriptions
 
+  has_many :reverse_subscriptions, foreign_key: :leader_id, class_name: "Subscription",
+                                                            dependent: :destroy
+  has_many :followers, through: :reverse_subscriptions
+
+  has_many :posts, dependent: :destroy
+  has_many :text_posts, dependent: :destroy
+  has_many :image_posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
   def following?(leader)
     leaders.include? leader
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150914094844) do
+ActiveRecord::Schema.define(version: 20150914170332) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.string   "url"
+    t.string   "type"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "subscriptions", force: :cascade do |t|
     t.integer  "leader_id"
