86a4964b62df554e53c4b46c774a6d45861cf956
tranhuyhoangbka <hoangth92.nd@gmail.com>
2015-09-14 17:43:28 +0700

    addtional method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   has_many :subscriptions, foreign_key: :follower_id, dependent: :destroy
   has_many :leaders, through: :subscriptions
+
+  def following?(leader)
+    leaders.include? leader
+  end
+
+  def follow!(leader)
+    if leader != self && !following?(leader)
+      leaders << leader
+    end
+  end
 end
